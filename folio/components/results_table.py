5e9e73706f709eda864ac0a9bee2475027c15973
Oisin Coveney <git@oisin.ee>
2026-07-02 02:00:32 +0300

    fix(e2e): fire parse start on table mount

--- folio/components/results_table.py
+++ folio/components/results_table.py
@@ -232,7 +232,6 @@ def _table_header() -> rx.Component:
 def results_table() -> rx.Component:
     """Render the two-column results view."""
     return rx.flex(
-        rx.box(on_mount=BatchState.start_pending_parse, display="none"),
         rx.flex(
             _table_header(),
             rx.box(
@@ -277,4 +276,5 @@ def results_table() -> rx.Component:
         flex_grow="1",
         overflow="hidden",
         align="stretch",
+        on_mount=BatchState.start_pending_parse,
     )
